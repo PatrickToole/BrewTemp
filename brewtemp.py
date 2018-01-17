025d1ed2f5bb4956c17477479691dd9cc9a3b3a1
PatrickToole <35511900+PatrickToole@users.noreply.github.com>
2018-01-16 22:37:31 -0400

    working logic

--- brewtemp.py
+++ brewtemp.py
@@ -1,14 +1,19 @@
-temp = getTemperatue()  #float(input('what is the temperature'))
-GLOBAL_maxTemp = 70
-GLOBAL_minTemp = 63
+import random
 
-logic(temp, GLOBAL_maxTemp, GLOBAL_minTemp)
+def getTemperature():
+    randNum = random.randrange(60,72)
+    print(randNum,'C', randNum*1.8+32,'F' )
+    return randNum
 
-def logicONOFF(temp, maxTemp, minTemp):
+def logic(temp, maxTemp, minTemp):
       if temp >= maxTemp:
           print('turn heat off')
       elif temp <= minTemp:
           print('turn heat on')
       else:
           print('maintain current heat')
-      print temp
+
+temp = getTemperature()  #float(input('what is the temperature'))
+GLOBAL_maxTemp = 68
+GLOBAL_minTemp = 64
+logic(temp, GLOBAL_maxTemp, GLOBAL_minTemp)
