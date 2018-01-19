0c565104136d21b6a1a50acbe2d51c38ac1c62e1
PatrickToole <35511900+PatrickToole@users.noreply.github.com>
2018-01-18 22:10:58 -0400

    trying to loop

--- brewtemp.py
+++ brewtemp.py
@@ -1,19 +1,28 @@
 import random
+import time
+def main():
+    t_end = time.time() + 60
 
-def getTemperature():
-    randNum = random.randrange(60,72)
-    print(randNum,'C', randNum*1.8+32,'F' )
-    return randNum
+    def getTemperature():
+        randNum = random.randrange(60,72)
+        print(randNum,'C', randNum*1.8+32,'F' )
+        return randNum
 
-def logic(temp, maxTemp, minTemp):
-      if temp >= maxTemp:
-          print('turn heat off')
-      elif temp <= minTemp:
-          print('turn heat on')
-      else:
-          print('maintain current heat')
 
-temp = getTemperature()  #float(input('what is the temperature'))
-GLOBAL_maxTemp = 68
-GLOBAL_minTemp = 64
-logic(temp, GLOBAL_maxTemp, GLOBAL_minTemp)
+    def logic(temp, maxTemp, minTemp):
+        if temp >= maxTemp:
+            print('turn heat off')
+        elif temp <= minTemp:
+            print('turn heat on')
+        else:
+            print('maintain current heat')
+    if t_end > time.time():
+        main()
+    else:
+        exit()
+
+    temp = getTemperature()  #float(input('what is the temperature'))
+    GLOBAL_maxTemp = 68
+    GLOBAL_minTemp = 64
+    logic(temp, GLOBAL_maxTemp, GLOBAL_minTemp)
+
