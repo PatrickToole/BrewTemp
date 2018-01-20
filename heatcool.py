d9ca5a44c3e617a5412b1ef794823bac396ed66d
PatrickToole <35511900+PatrickToole@users.noreply.github.com>
2018-01-20 13:34:14 -0400

    print(temp) time.sleep()
    rather than
    
    time.sleep()
    print(temp)

--- heatcool.py
+++ heatcool.py
@@ -1,7 +1,7 @@
 import time
 
 # represents 1 hour in a minute. to change to 1 hour make end time 60 and sleep timers 15. well i think lol
-end_time = 1                            #will gather input time, in minutes, from the GUI
+end_time = .1                            #will gather input time, in minutes, from the GUI
 start_time = time.time()
 
 temp = 55
@@ -14,12 +14,14 @@ while (time.time() - start_time) < (end_time * 60):
 
     while temp < target_temp:
         temp += temp_delta_heat
-        time.sleep(0)
         print(temp)
+        time.sleep(0)
+
     while temp > target_temp:
         temp -= temp_delta_cool
-        time.sleep(0)
         print(temp)
+        time.sleep(0)
+
 
 
 
