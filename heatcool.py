e438cc94f8da94204549d66c20fba7782184747e
PatrickToole <35511900+PatrickToole@users.noreply.github.com>
2018-01-20 13:18:11 -0400

    added heatcool and i think i have modeled the mash temperature heating and cooling schedule. with estimates on heat loss and heat gained using calculations from the internet.

--- heatcool.py
+++ heatcool.py
@@ -1,15 +1,17 @@
 import time
 
-target_temp = 67
-temp = 55
-
 # represents 1 hour in a minute. to change to 1 hour make end time 60 and sleep timers 15. well i think lol
 end_time = 1                            #will gather input time, in minutes, from the GUI
 start_time = time.time()
 
+temp = 55
+target_temp = 67
+
 temp_delta_heat = (82.165/3600*15)
-temp_delta_cool = (15/23.39)
+temp_delta_cool = (15/120)
+
 while (time.time() - start_time) < (end_time * 60):
+
     while temp < target_temp:
         temp += temp_delta_heat
         time.sleep(0)
@@ -18,6 +20,7 @@ while (time.time() - start_time) < (end_time * 60):
         temp -= temp_delta_cool
         time.sleep(0)
         print(temp)
+
 
 
 
